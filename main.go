8896fcc2826c4c918b11ca05cd09fb5c08eb9988
justinlime <justinlime1999@gmail.com>
2023-11-05 21:02:40 -0600

    edit

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
     "github.com/bwmarrin/discordgo"
 
     "bufio"
+    "flag"
     "fmt"
     "io"
     "log"
     "os"
-    "os/signal"
-    "syscall"
 )
 
-var Message string
+var(
+    Message string
+    Token string
+    Channel string
+)      
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.StringVar(&Channel, "c", "", "Channel")
+	flag.Parse()
+}
 
 func main() {
     nBytes, nChunks := int64(0), int64(0)
@@ -48,44 +57,29 @@ func main() {
         }
     }
 
-    fmt.Println("Bytes:", nBytes, "Chunks:", nChunks)
+    // fmt.Println("Bytes:", nBytes, "Chunks:", nChunks)
 }
 
 func sendToBot(){
-    dg, err := discordgo.New("Bot " + "")
+    if Token == "" {
+        log.Fatal("No token passed, pass one with the -t flag") 
+        return
+    }    
+    if Channel == "" {
+        log.Fatal("No ChannelID passed, pass one with the -c flag")
+        return
+    } 
+    dg, err := discordgo.New("Bot " + Token)
     if err != nil {
         fmt.Printf("Error!")
     }    
-    dg.AddHandler(messageCreate)
     dg.Identify.Intents = discordgo.IntentsGuildMessages
  	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+    dg.ChannelMessageSend(Channel, Message)
 
     dg.Close()  
 }      
-
- func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, Message)
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
-}
